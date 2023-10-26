f9f01820d90177b83ce90e2dcc8ad51bf67ca2ce
RAZ0R <shaggi199712@gmail.com>
2023-10-26 16:17:18 +0530

    Test workflow

--- tests/githubCI.py
+++ tests/githubCI.py
@@ -2,6 +2,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 userName = os.environ['BROWSERSTACK_USERNAME']
 accessKey = os.environ['BROWSERSTACK_ACCESS_KEY']
